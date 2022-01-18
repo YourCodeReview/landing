e56264b699627b203d35ece194c20b1d05527fe1
vandrienko <vandrienko@varteq.com>
2022-01-18 14:55:06 +0200

    change price

--- src/js/index.js
+++ src/js/index.js
@@ -1,19 +1,19 @@
-import Header from '../blocks/modules/header/header';
-import Form from '../blocks/modules/form/form';
-import ScrollTo from './import/scrollTo';
-import checkAnchor from './import/checkAnchor';
-import ToggleElement from './import/ToggleElement';
+import Header from "../blocks/modules/header/header";
+import Form from "../blocks/modules/form/form";
+import ScrollTo from "./import/scrollTo";
+import checkAnchor from "./import/checkAnchor";
+import ToggleElement from "./import/ToggleElement";
 
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
   checkAnchor();
   Header.init();
   ScrollTo.init();
   //form
-  document.querySelectorAll('.welcome-form').forEach($this => {
+  document.querySelectorAll(".welcome-form").forEach($this => {
     new Form($this).init();
-  })
+  });
   //toggle
-  document.querySelectorAll('[data-toggle="parent"]').forEach($this => {
+  document.querySelectorAll("[data-toggle=\"parent\"]").forEach($this => {
     new ToggleElement($this).init();
-  })
-})
+  });
+});
