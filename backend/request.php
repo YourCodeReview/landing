36af9f7c59c8ca1967aeb843d2e4d3afae78c3e1
vandrienko <vandrienko@varteq.com>
2022-03-20 16:50:38 +0200

    change pricing & phone validation

--- backend/request.php
+++ backend/request.php
@@ -34,13 +34,13 @@ function sendEmail(string $email, string $subject, string $message): bool {
 
 try {
 	$telegram = filter_input(INPUT_POST, 'telegram', FILTER_SANITIZE_STRING);
-    $telegram = filter_var(
+    /*$telegram = filter_var(
         $telegram, 
         FILTER_VALIDATE_REGEXP,
         [
             'options' => ['regexp' => "#^(https:\/\/t\.me\/|@)?[\w]{5,}|\+\d{7,}$#"],
         ]
-    );
+    );*/
 	if (empty($telegram)) {
 		throw new Exception('"telegram" is not valid!');
 	}
